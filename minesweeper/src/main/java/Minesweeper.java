0fc2de1808be09bbd85481117b57b942d7fd01d6
Freddy Chiron <fchiron.ext@orange.com>
2016-03-22 11:16:16 +0100

    commit first version

--- minesweeper/src/main/java/Minesweeper.java
+++ minesweeper/src/main/java/Minesweeper.java
@@ -1,3 +1,96 @@
+import java.util.List;
+
+import org.assertj.core.util.Lists;
+
 public class Minesweeper {
 
+	private static final String BOMB = "*";
+	private String[][] field;
+	private String[][] countedMines;
+
+	public Minesweeper(int nbLines, int nbColumns) {
+		super();
+		field = new String[nbLines][nbColumns];
+		countedMines = new String[nbLines][nbColumns];
+
+	}
+
+	public String[][] getCountedMines() {
+		for (int line = 0; line < field.length; line++) {
+			for (int column = 0; column < field[line].length; column++) {
+				this.countedMines[line][column] = getMineOrCountedNeighborsMinesForACell(line, column);
+			}
+		}
+
+		return this.countedMines;
+	}
+
+	private String getMineOrCountedNeighborsMinesForACell(int line, int column) {
+		if (BOMB.equals(field[line][column])) {
+			return BOMB;
+		}
+		return getCountedNeighborMines(line, column);
+	}
+
+	private String getCountedNeighborMines(int line, int column) {
+
+		List<String> neighborsValues = collectNeigbors(line, column);
+
+		int countedNeighborsMines = 0;
+		for (String neighborValue : neighborsValues) {
+			if (BOMB.equals(neighborValue)) {
+				countedNeighborsMines++;
+			}
+		}
+
+		return String.valueOf(countedNeighborsMines);
+	}
+
+	private List<String> collectNeigbors(int line, int column) {
+
+		List<String> neighbors = Lists.newArrayList();
+		neighbors.add(getNeighborValue(line, column - 1));
+		neighbors.add(getNeighborValue(line, column + 1));
+		neighbors.add(getNeighborValue(line - 1, column));
+		neighbors.add(getNeighborValue(line + 1, column));
+		neighbors.add(getNeighborValue(line - 1, column + 1));
+		neighbors.add(getNeighborValue(line + 1, column - 1));
+		neighbors.add(getNeighborValue(line - 1, column - 1));
+		neighbors.add(getNeighborValue(line + 1, column + 1));
+		return neighbors;
+	}
+
+	private String getNeighborValue(int line, int column) {
+		boolean neighborOutOfField = line < 0 || column < 0 || line > field.length - 1 || column > field[field.length - 1].length - 1;
+		if (neighborOutOfField) {
+			return null;
+		}
+		return field[line][column];
+	}
+
+	public static String display(String[][] field) {
+		StringBuilder displayField = new StringBuilder();
+
+		for (String[] line : field) {
+			for (String element : line) {
+				displayField.append(element);
+			}
+			displayField.append("\n");
+		}
+		displayField.deleteCharAt(displayField.length() - 1);
+		return displayField.toString();
+
+	}
+
+	public void initializeFieldWith(String[][] outputField) {
+		for (int line = 0; line < outputField.length; line++) {
+			for (int column = 0; column < outputField[line].length; column++) {
+				this.field[line][column] = outputField[line][column];
+			}
+		}
+	}
+
+	public Object getFieldValue(int line, int column) {
+		return field[line][column];
+	}
 }
--- minesweeper/src/test/java/MinesweeperTest.java
+++ minesweeper/src/test/java/MinesweeperTest.java
@@ -1,3 +1,91 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
 public class MinesweeperTest {
 
+	@Test
+	public void initializeWith_should_return_field_initialized_given_a_line_field() {
+
+		Minesweeper minesweeper = new Minesweeper(1, 2);
+		minesweeper.initializeFieldWith(new String[][] { { "*", "." } });
+
+		assertThat(minesweeper.getFieldValue(0, 0)).isEqualTo("*");
+		assertThat(minesweeper.getFieldValue(0, 1)).isEqualTo(".");
+	}
+
+	@Test
+	public void initializeWith_should_return_field_initialized_given_a_2_x_2_field() {
+
+		//Given
+		Minesweeper minesweeper = new Minesweeper(2, 2);
+
+		//When
+		minesweeper.initializeFieldWith(new String[][] { { "*", "." }, { ".", "*" } });
+
+		// Then
+		assertThat(minesweeper.getFieldValue(0, 0)).isEqualTo("*");
+		assertThat(minesweeper.getFieldValue(0, 1)).isEqualTo(".");
+
+		assertThat(minesweeper.getFieldValue(1, 0)).isEqualTo(".");
+		assertThat(minesweeper.getFieldValue(1, 1)).isEqualTo("*");
+	}
+
+	@Test
+	public void getCountedMines_should_return_star_one_given_a_line_field_with_star_dot() {
+
+		//Given
+		Minesweeper minesweeper = new Minesweeper(1, 2);
+		minesweeper.initializeFieldWith(new String[][] { { "*", "." } });
+
+		// Test
+		String[][] countedMines = minesweeper.getCountedMines();
+
+		// Then
+		assertThat(Minesweeper.display(countedMines)).isEqualTo("*1");
+	}
+
+	@Test
+	public void getCountedMines_should_return_star_two_given_a_line_field_with_star_dot_star() {
+
+		//Given
+		Minesweeper minesweeper = new Minesweeper(1, 3);
+		minesweeper.initializeFieldWith(new String[][] { { "*", ".", "*" } });
+
+		// Test
+		String[][] countedMines = minesweeper.getCountedMines();
+
+		// Then
+		assertThat(Minesweeper.display(countedMines)).isEqualTo("*2*");
+	}
+
+	@Test
+	public void getCountedMines_should_return_ok() {
+
+		//Given
+		Minesweeper minesweeper = new Minesweeper(2, 3);
+		minesweeper.initializeFieldWith(new String[][] { { "*", ".", "*" }, { "*", ".", "*" } });
+
+		// Test
+		String[][] countedMines = minesweeper.getCountedMines();
+
+		// Then
+		assertThat(Minesweeper.display(countedMines)).isEqualTo("*4*\n*4*");
+	}
+
+	@Test
+	public void getCountedMines_should_return_ok_given_initial_output() {
+
+		//Given
+		Minesweeper minesweeper = new Minesweeper(4, 4);
+		minesweeper.initializeFieldWith(new String[][] { { "*", ".", ".", "." }, { ".", ".", ".", "." }, { ".", "*", ".", "." },
+				{ ".", ".", ".", "." } });
+
+		// Test
+		String[][] countedMines = minesweeper.getCountedMines();
+
+		// Then
+		assertThat(Minesweeper.display(countedMines)).isEqualTo("*100\n2210\n1*10\n1110");
+
+	}
 }
